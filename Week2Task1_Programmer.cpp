49177513a9bd9c499fc463ffdf6048d4f1572abf
DashaSobolevskaya <awanijima@outlook.com>
2015-09-29 10:12:42 +0300

    Update Week2Task1_Programmer.cpp

--- Week2Task1_Programmer.cpp
+++ Week2Task1_Programmer.cpp
@@ -15,9 +15,10 @@ int main()
 	//cin >> epselon;
 	//cout << "\nmySin(x):\t" << mySin(x,epselon);
 	assert(3.14, 0.001);
-	assert(3.14, 0.001);
-	assert(3.14, 0.001);
-	assert(3.14, 0.001);
+	assert(1, 0.01);
+	assert(3467, 2);
+	assert(123456, 0.1);
+	assert(1234566756, 0.000001);
 	system("pause");
 	return 0;
 }
