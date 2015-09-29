4a9209990bb3ca12fa760a93d1ef071b09a81c6b
DashaSobolevskaya <awanijima@outlook.com>
2015-09-29 10:11:12 +0300

    Update Week2Task1_Programmer.cpp

--- Week2Task1_Programmer.cpp
+++ Week2Task1_Programmer.cpp
@@ -3,16 +3,21 @@
 using namespace std;
 
 inline float mySin(float,float);
+void assert(float, float);
 
 int main()
 {
-	cout << "Enter x:\t";
+	//cout << "Enter x:\t";
 	float x;
-	cin >> x;
-	cout << "\nEnter accurace epselon:\t";
+	//cin >> x;
+	//cout << "\nEnter accurace epselon:\t";
 	float epselon;
-	cin >> epselon;
-	cout << "\nmySin(x):\t" << mySin(x,epselon);
+	//cin >> epselon;
+	//cout << "\nmySin(x):\t" << mySin(x,epselon);
+	assert(3.14, 0.001);
+	assert(3.14, 0.001);
+	assert(3.14, 0.001);
+	assert(3.14, 0.001);
 	system("pause");
 	return 0;
 }
@@ -28,4 +33,12 @@ inline float mySin(float x,float epselon)
 		member *= (x*x)/(++n)/(++n);
 	}
 	return answer;
+}
+
+void assert(float x, float eps)
+{
+	if (fabs (mySin(x, eps) - sin(x)) <= eps)
+		cout << "OK\n";
+	else cout << "FAIL\n";
+	
 }
