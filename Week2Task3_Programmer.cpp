d15cbcc446b694304d246afb8421059da4c0b149
DashaSobolevskaya <awanijima@outlook.com>
2015-09-29 10:20:28 +0300

    Update Week2Task3_Programmer.cpp

--- Week2Task3_Programmer.cpp
+++ Week2Task3_Programmer.cpp
@@ -2,13 +2,19 @@
 using namespace std;
 
 inline int amountOfZero(int);
+void assert(int, int);
 
 int main()
 {
-    cout << "Enter a number:\t";
+    //cout << "Enter a number:\t";
     int number;
-    cin >> number;
-    cout << "Amount of zero in the end of the number! is:\t" << amountOfZero(number); 
+    //cin >> number;
+    //cout << "Amount of zero in the end of the number! is:\t" << amountOfZero(number);
+    assert(5, 1);
+    assert(25, 6);
+    assert(125, 31);
+    assert(75, 18);
+    assert(1, 0);
     system("pause");
     return 0;
 }
@@ -22,4 +28,10 @@ int amountOfZero(int num)
         num /= divider;
     }
     return ans;
+}
+
+void assert(int num, int answer)
+{
+    if (answer == amountOfZero(num)) cout << "OK\n";
+    else cout << "FAIL\n";
 }
